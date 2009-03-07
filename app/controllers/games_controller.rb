7461790804bfebbc79a46291a8e3b73010d24763
Guoliang Cao <gcao@vonage.com>
2009-03-07 17:45:28 -0500

    Added calendar support

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,11 @@
 class GamesController < ApplicationController
-  active_scaffold :game
+  active_scaffold :game do |config|
+    config.columns = [:name, :event, :place, :source, :played_at,
+      :rule, :komi, :result, :winner, :moves,
+      :black_id, :black_name, :black_rank,
+      :white_id, :white_name, :white_rank, :description]
+    config.list.columns = [:event, :name, :played_at,
+      :black_name, :black_rank, :white_name, :white_rank, :rule, :komi, :result, :winner, :moves]
+    config.nested.add_link "Content", [:game_data]
+  end
 end
