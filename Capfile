2553b373088f9a495b93d0141ffc1cd93dc41b4a
Guoliang Cao <gcao@vonage.com>
2009-08-05 09:07:17 -0400

    Trying to setup EC2 based deployment

--- Capfile
+++ Capfile
@@ -1,3 +1,13 @@
 load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 load 'config/deploy'
+
+after "deploy:update_code", :copy_over_config_files
+
+task :copy_over_config_files do
+  run "for config_file in #{deploy_to}/#{shared_dir}/config/*; do ln -nfs $config_file #{release_path}/config/`basename $config_file`; done"
+end
+
+# load 'deploy' if respond_to?(:namespace) # cap2 differentiator
+# load 'config/deploy'
+# require 'ec2onrails/recipes'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,81 +1,121 @@
-#############################################################
-#  Application
-#############################################################
- 
 set :application, "gocool"
-set :deploy_to, "/deploy/gocool"
- 
-#############################################################
-#  Settings
-#############################################################
- 
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
-set :use_sudo, true
-set :scm_verbose, true
-set :rails_env, "production"
- 
-#############################################################
-#  Servers
-#############################################################
- 
-set :user, "gcao"
-set :domain, "209.20.88.80"
-server domain, :app, :web
-role :db, domain, :primary => true
- 
-#############################################################
-#  Git
-#############################################################
+set :deploy_to, "/data/apps/#{application}"
  
 set :scm, :git
-set :branch, "master"
-set :scm_user, 'gcao'
-set :scm_passphrase, ""
 set :repository, "git@github.com:gcao/gocool.git"
-set :deploy_via, :remote_cache
  
-#############################################################
-#  Passenger
-#############################################################
- 
-namespace :deploy do
-  desc "Create the database yaml file"
-  task :after_update_code do
-    db_config = <<-EOF
-production:
-adapter: mysql
-encoding: utf8
-username: root
-password:
-database: gocool_production
-host: localhost
-EOF
-    
-    put db_config, "#{release_path}/config/database.yml"
-    
-    #########################################################
-    # Uncomment the following to symlink an uploads directory.
-    # Just change the paths to whatever you need.
-    #########################################################
-    
-    # desc "Symlink the upload directories"
-    # task :before_symlink do
-    # run "mkdir -p #{shared_path}/uploads"
-    # run "ln -s #{shared_path}/uploads #{release_path}/public/uploads"
-    # end
-  
-  end
-    
-  # Restart passenger on deploy
-  desc "Restarting mod_rails with restart.txt"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-  
-  [:start, :stop].each do |t|
-    desc "#{t} task is a no-op with mod_rails"
-    task t, :roles => :app do ; end
-  end
-  
-end
+set :user, "gcao"
+set :use_sudo, false
+
+# AMI ami-0d729464: ubuntu 9.04 server base 
+server %x(ami_host).strip, :app, :web, :db, :primary => true
+
+# # This is a sample Capistrano config file for EC2 on Rails.
+# # It should be edited and customized.
+# 
+# set :application, "gocool"
+# 
+# set :scm, :git
+# set :branch, "master"
+# set :scm_user, 'gcao'
+# set :scm_passphrase, ""
+# set :repository, "git@github.com:gcao/gocool.git"
+# set :deploy_via, :remote_cache
+# 
+# # NOTE: for some reason Capistrano requires you to have both the public and
+# # the private key in the same folder, the public key should have the 
+# # extension ".pub".
+# ssh_options[:keys] = ["#{ENV['HOME']}/.ec2/amazon.pem"]
+# 
+# # Your EC2 instances. Use the ec2-xxx....amazonaws.com hostname, not
+# # any other name (in case you have your own DNS alias) or it won't
+# # be able to resolve to the internal IP address.
+# set :ec2_host, "ec2-12-xx-xx-xx.z-1.compute-1.amazonaws.com"
+# role :web,      ec2_host
+# role :memcache, ec2_host
+# role :db,       ec2_host, :primary => true
+# # role :db,       "ec2-56-xx-xx-xx.z-1.compute-1.amazonaws.com", :primary => true, :ebs_vol_id => 'vol-12345abc'
+# # optinally, you can specify Amazon's EBS volume ID if the database is persisted 
+# # via Amazon's EBS.  See the main README for more information.
+# 
+# # Whatever you set here will be taken set as the default RAILS_ENV value
+# # on the server. Your app and your hourly/daily/weekly/monthly scripts
+# # will run with RAILS_ENV set to this value.
+# set :rails_env, "production"
+# 
+# # EC2 on Rails config. 
+# # NOTE: Some of these should be omitted if not needed.
+# set :ec2onrails_config, {
+#   # S3 bucket and "subdir" used by the ec2onrails:db:restore task
+#   # NOTE: this only applies if you are not using EBS
+#   :restore_from_bucket => "gocool_db",
+#   :restore_from_bucket_subdir => "database",
+#   
+#   # S3 bucket and "subdir" used by the ec2onrails:db:archive task
+#   # This does not affect the automatic backup of your MySQL db to S3, it's
+#   # just for manually archiving a db snapshot to a different bucket if 
+#   # desired.
+#   # NOTE: this only applies if you are not using EBS
+#   :archive_to_bucket => "gocool_db_archive",
+#   :archive_to_bucket_subdir => "db-archive/#{Time.new.strftime('%Y-%m-%d--%H-%M-%S')}",
+#   
+#   # Set a root password for MySQL. Run "cap ec2onrails:db:set_root_password"
+#   # to enable this. This is optional, and after doing this the
+#   # ec2onrails:db:drop task won't work, but be aware that MySQL accepts 
+#   # connections on the public network interface (you should block the MySQL
+#   # port with the firewall anyway). 
+#   # If you don't care about setting the mysql root password then remove this.
+#   #:mysql_root_password => "your-mysql-root-password",
+#   
+#   # Any extra Ubuntu packages to install if desired
+#   # If you don't want to install extra packages then remove this.
+#   #:packages => ["logwatch", "imagemagick"],
+#   
+#   # Any extra RubyGems to install if desired: can be "gemname" or if a 
+#   # particular version is desired "gemname -v 1.0.1"
+#   # If you don't want to install extra rubygems then remove this
+#   # NOTE: if you are using rails 2.1, ec2onrails calls 'sudo rake gem:install',
+#   # which will install gems defined in your rails configuration
+#   #:rubygems => ["rmagick", "rfacebook -v 0.9.7"],
+#   
+#   # extra security measures are taken if this is true, BUT it makes initial
+#   # experimentation and setup a bit tricky.  For example, if you do not
+#   # have your ssh keys setup correctly, you will be locked out of your
+#   # server after 3 attempts for upto 3 months.  
+#   :harden_server => false,
+#   
+#   #if you want to harden the server, or setup email signing, you will need to set the domain
+#   #if you use Capistrano's multistage extension (recommended!), you can add a line like this to your
+#   #environment specific file:
+#   #      ec2onrails_config[:service_domain] = 'staging.mydomain.com'
+#   :service_domain => nil,
+#   
+#   # Set the server timezone. run "cap -e ec2onrails:server:set_timezone" for 
+#   # details
+#   :timezone => "UTC",
+#   
+#   # Files to deploy to the server (they'll be owned by root). It's intended
+#   # mainly for customized config files for new packages installed via the 
+#   # ec2onrails:server:install_packages task. Subdirectories and files inside
+#   # here will be placed in the same structure relative to the root of the
+#   # server's filesystem. 
+#   # If you don't need to deploy customized config files to the server then
+#   # remove this.
+#   :server_config_files_root => "../server_configs",
+#   
+#   # If config files are deployed, some services might need to be restarted.
+#   # If you don't need to deploy customized config files to the server then
+#   # remove this.
+#   :services_to_restart => %w(postfix sysklogd),
+#   
+#   # Set an email address to forward admin mail messages to. If you don't
+#   # want to receive mail from the server (e.g. monit alert messages) then
+#   # remove this.
+#   #:mail_forward_address => nil,
+#   
+#   # Set this if you want SSL to be enabled on the web server. The SSL cert 
+#   # and key files need to exist on the server, The cert file should be in
+#   # /etc/ssl/certs/default.pem and the key file should be in
+#   # /etc/ssl/private/default.key (see :server_config_files_root).
+#   :enable_ssl => false
+# }
