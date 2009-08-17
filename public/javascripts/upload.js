383182fe98c6a721628e89a9cd1a0aba8ed1acb9
Guoliang Cao <gcao@vonage.com>
2009-08-17 08:48:09 -0400

    Upload more than one file in one shot

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,7 +1,11 @@
 function addUploadField() {
   var fieldId = 'upload_upload' + Math.floor(Math.random()*1000);
-  jQuery('#file_fields').append('<input id="' + fieldId + '" name="upload[upload]" size="30" type="file" />');
+  jQuery('#file_fields').append('<div id="' + fieldId + '"><input name="upload[upload]" size="60" type="file" /><input type="button" value="Delete This" onclick="removeUploadField(\'' + fieldId + '\')"/></div>');
   jQuery('#' + fieldId).change(addUploadField);
+}
+
+function removeUploadField(id) {
+  jQuery('#' + id).fadeOut(function(){jQuery(this).remove();})
 }
 
 jQuery(document).ready(function(){
