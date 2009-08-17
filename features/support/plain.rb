effdb6c3ee6782c7c453b3ffc61569a64b8dbe53
Guoliang Cao <gcao@vonage.com>
2009-08-17 09:12:36 -0400

    Setting up cucumber+selenium

---
+++ features/support/plain.rb
@@ -0,0 +1,6 @@
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+# truncate your tables here if you are using the same database as selenium, since selenium doesn't use transactional fixtures
+Cucumber::Rails.use_transactional_fixtures
