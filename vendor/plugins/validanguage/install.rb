d47f79ca5a71984cc4fa3cb8eba926b91dae7c66
Guoliang Cao <gcao@vonage.com>
2009-10-09 17:36:28 -0400

    Add show_log to show all logs related to gocool when access through apache

--- vendor/plugins/validanguage/install.rb
+++ vendor/plugins/validanguage/install.rb
@@ -1,14 +1,14 @@
-# Install hook code here
+# Rails plugin install hook code here
 puts "Copying files..."
 {
-  "validanguage.js" => "/public/javascripts/validanguage.js",
-  "validanguage_uncompressed.js" => "/public/javascripts/validanguage_uncompressed.js",
-  "ajax-loader.gif" => "/public/images/ajax-loader.gif",
-  "red_error.gif" => "/public/images/red_error.gif",
-  "validanguage.css" => "/public/stylesheets/validanguage.css"
+  "validanguage.js"              => %w(public javascripts validanguage.js),
+  "validanguage_uncompressed.js" => %w(public javascripts validanguage_uncompressed.js),
+  "ajax-loader.gif"              => %w(public images ajax-loader.gif),
+  "red_error.gif"                => %w(public images red_error.gif),
+  "validanguage.css"             => %w(public stylesheets validanguage.css)
 }.each do |src, dest|
-  src = File.dirname(__FILE__) + "/" + src
-  dest = RAILS_ROOT + "/" + dest
+  src  = File.join(File.dirname(__FILE__), src)
+  dest = File.join(RAILS_ROOT, dest)
   puts "#{src} => #{dest}"
 	FileUtils.cp_r(src, dest)
 end
