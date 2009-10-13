9eac97c2091d2916d35f01c93adbe6afa551d6ae
Guoliang Cao <gcao@vonage.com>
2009-10-13 17:21:29 -0400

    Deployment configuration changes

--- Capfile
+++ Capfile
@@ -2,9 +2,9 @@ load 'deploy' if respond_to?(:namespace) # cap2 differentiator
 Dir['vendor/plugins/*/recipes/*.rb'].each { |plugin| load(plugin) }
 load 'config/deploy'
 
-after "deploy:update_code", :copy_over_config_files
-
-task :copy_over_config_files do
-  run "for config_file in #{deploy_to}/#{shared_dir}/config/*; do ln -nfs $config_file #{release_path}/config/`basename $config_file`; done"
-end
+# after "deploy:update_code", :copy_over_config_files
+# 
+# task :copy_over_config_files do
+#   run "for config_file in #{deploy_to}/#{shared_dir}/config/*; do ln -nfs $config_file #{release_path}/config/`basename $config_file`; done"
+# end
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,23 @@ set :application, "gocool"
 set :deploy_to, "/data/apps/#{application}"
  
 set :scm, :git
-set :repository, "git@github.com:gcao/gocool.git"
+set :repository, "git://github.com/gcao/gocool.git"
+set :git_enable_submodules, true
  
-set :user, "gcao"
+set :user, "root"
 set :use_sudo, false
 
 # AMI ami-0d729464: ubuntu 9.04 server base 
-server %x(ami_host).strip, :app, :web, :db, :primary => true
+server "ec2-67-202-52-61.compute-1.amazonaws.com", :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start do
+  end
+  
+  task :stop do
+  end
+  
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    try_runner "touch #{current_path}/tmp/restart.txt"
+  end
+end
