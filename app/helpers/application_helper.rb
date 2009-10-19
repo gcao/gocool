7ebe62cb56c2c992059532ed0f60cc6001cc0e86
Guoliang Cao <gcao@vonage.com>
2009-10-19 09:20:29 -0400

    Games of player

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
   def show_flash container_class
     render :partial => 'shared/show_flash', :locals => {:container_class => container_class}
   end
+  
+  def black_player_html(game)
+    css_class = "winner" if game.winner == Game::WINNER_BLACK
+    "<td class='#{css_class}'>#{game.black_name_with_rank}</td>"
+  end
+  
+  def white_player_html(game)
+    css_class = "winner" if game.winner == Game::WINNER_WHITE
+    "<td class='#{css_class}'>#{game.white_name_with_rank}</td>"
+  end
 end
