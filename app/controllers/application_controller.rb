8abe3280aff6a6bb3c6a9af1a369598148c35dcf
Guoliang Cao <gcao@vonage.com>
2009-12-05 20:48:52 -0500

    Integrate with Discuz forum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   # protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  # layout 'application_integrated'
+
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password
   
