213f3ad07ed04d549867aec9a1c958847b7f7a1c
Guoliang Cao <gcao@vonage.com>
2009-12-11 14:45:21 -0500

    Trying to use asset host to serve static assets

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,3 +3,8 @@ require 'compass'
 Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
+
+# Uncomment this on production server
+#asset_host do |asset|
+#  "http://assets%d.go-cool.org" % (asset.hash % 4)
+#end
