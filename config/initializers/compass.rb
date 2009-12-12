6953533e19e3aeaae301e515962c755fd0b36de1
Guoliang Cao <gcao@vonage.com>
2009-12-12 08:39:52 -0500

    Fix compass config

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -3,8 +3,3 @@ require 'compass'
 Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
 Compass.configure_sass_plugin!
-
-# Uncomment this on production server
-#asset_host do |asset|
-#  "http://assets%d.go-cool.org" % (asset.hash % 4)
-#end
