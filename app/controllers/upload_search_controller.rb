b7d89f1fc06dd055752eafc8e1a07cdb2b1a6631
Guoliang Cao <gcao@vonage.com>
2010-01-06 10:05:36 -0500

    Search upload by dates

--- app/controllers/upload_search_controller.rb
+++ app/controllers/upload_search_controller.rb
@@ -1,8 +1,18 @@
 class UploadSearchController < ApplicationController
   def index
-    if params[:search_type] == 'description'
-      description = params[:description].try(:strip)
-      @uploads = Upload.with_description(description).paginate(page_params)
+    if search_by_description?
+      @description = params[:description].try(:strip)
+      @uploads = Upload.with_description(@description).paginate(page_params)
     end
   end
+
+  def search_by_description?
+    params[:search_type] == 'description'
+  end
+
+  def search_by_date?
+    params[:search_type] == 'date'
+  end
+  
+  helper_method :search_by_description?, :search_by_date?
 end
