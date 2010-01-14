a56c73d11371a5c24fc723a04b8c4476af673e48
Guoliang Cao <gcao@vonage.com>
2010-01-14 11:04:50 -0500

    Fix class method

--- app/models/common_game_stat.rb
+++ app/models/common_game_stat.rb
@@ -1,11 +1,17 @@
 module CommonGameStat
-  def find_or_create player_id, opponent_id
-    if player_id > opponent_id
-      player_id, opponent_id = opponent_id, player_id
-    elsif player_id == opponent_id
-      raise "Player and opponent ID are the same: #{player_id}"
-    end
+  def self.included(klass)
+    klass.extend CommonGameStat::ClassMethods
+  end
+  
+  module ClassMethods
+    def find_or_create player_id, opponent_id
+      if player_id > opponent_id
+        player_id, opponent_id = opponent_id, player_id
+      elsif player_id == opponent_id
+        raise "Player ID and opponent ID are the same: #{player_id}"
+      end
 
-    find_by_player_id_and_opponent_id(player_id, opponent_id) || create!(:player_id => player_id, :opponent_id => opponent_id)
+      find_by_player_id_and_opponent_id(player_id, opponent_id) || create!(:player_id => player_id, :opponent_id => opponent_id)
+    end
   end
 end
