7b1ac21b84af0e81842d70e76a20c8fe328c2435
Guoliang Cao <gcao@vonage.com>
2010-01-18 17:56:57 -0500

    Changes related to IE and to promote Firefox

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+jQuery(document).ready(function(){
+  if (jQuery.browser.mozilla) jQuery('#firefox_container').hide();
+});
+
 function changeLocale(locale){
   if (gocool_locale == locale) return;
   
