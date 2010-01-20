9c9e90319920e41e89cc022d06e8f137459c4cfa
Guoliang Cao <gcao@vonage.com>
2010-01-20 10:19:21 -0500

    Player+opponent page

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :uploads
   map.resources :pasties
   map.resources :players, :collection => {:search => :get}
+  map.resources :pairs
   map.resources :online_players, :collection => {:search => :get}
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
