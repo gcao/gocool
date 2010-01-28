acaf7981167727376c93a15f2e18b0b5362ab6a0
Guoliang Cao <gcao@vonage.com>
2010-01-28 09:52:27 -0500

    Remove exception notifier

--- config/initializers/exception_notification.rb
+++
@@ -1,7 +0,0 @@
-ExceptionNotifier.exception_recipients = %w(gcao99@gmail.com)
-
-# defaults to exception.notifier@default.com
-ExceptionNotifier.sender_address = %("Application Error" <admin@go-cool.org>)
-
-# defaults to "[ERROR] "
-ExceptionNotifier.email_prefix = "[GOCOOL] "
