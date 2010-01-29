0b21f6eb89039a7e4c3cc9ed3c667ba1b7834c84
Guoliang Cao <gcao@vonage.com>
2010-01-29 15:14:48 -0500

    Fix production problem

--- app/helpers/player_widget_helper.rb
+++ app/helpers/player_widget_helper.rb
@@ -8,6 +8,6 @@ module PlayerWidgetHelper
 
   def player_widget_header
     platform_name = (@player_for_widget.is_a? OnlinePlayer) ? @player_for_widget.gaming_platform.name : ""
-    t('players.games_played_by').gsub('PLAYER_NAME', @player_for_widget.name).gsub('PLATFORM_NAME', platform_name)
+    t('players.games_played_by').gsub('PLAYER_NAME', @player_for_widget.name.to_s).gsub('PLATFORM_NAME', platform_name)
   end
 end
