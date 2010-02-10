28fc1b0213a9e02ae51a078b06474b3a00c3b5e7
Guoliang Cao <gcao@vonage.com>
2010-02-10 09:22:49 -0500

    Change layout to make it work with admin pages

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,4 +2,9 @@ class Admin::BaseController < ApplicationController
 #  layout 'admin'
 
   before_filter lambda{ raise 'NOT ENABLED' if RAILS_ENV != 'development' }
+  before_filter :set_admin_flag
+
+  def set_admin_flag
+    @admin_page = true
+  end
 end
