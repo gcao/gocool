05b3dfbb070db1fc8c3592904880d06a25180ec9
Guoliang Cao <gcao@vonage.com>
2010-03-27 16:16:53 -0400

    Change favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ config.action_controller.perform_caching             = true
 # config.cache_store = :mem_cache_store
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
-config.action_controller.asset_host = "http://assets%d.go-cool.org"
+#config.action_controller.asset_host = "http://assets%d.go-cool.org"
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
