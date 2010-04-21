5e5fac62faed0f24ee66f0e8b061a7531247a3f3
Guoliang Cao <gcao@vonage.com>
2010-04-21 11:08:42 -0400

    Changed discuz integration module to gem and include as submodule

--- vendor/plugins/discuz_auth/app/models/discuz/member.rb
+++
@@ -1,4 +0,0 @@
-class Discuz::Member < Discuz::Base
-  set_table_name ENV['DISCUZ_TABLE_PREFIX'] + "_members"
-  set_primary_key 'uid'
-end
--- vendor/plugins/discuz_auth/app/models/discuz/session.rb
+++
@@ -1,6 +0,0 @@
-module Discuz
-  class Session < Discuz::Base
-    set_table_name ENV['DISCUZ_TABLE_PREFIX'] + "_sessions"
-    set_primary_key 'sid'
-  end
-end
