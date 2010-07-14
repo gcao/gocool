5491c4e84e956972192c6a26f3c910900e32fc51
Guoliang Cao <gcao@vonage.com>
2010-07-14 16:30:12 -0400

    Update statistics

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100708211622) do
+ActiveRecord::Schema.define(:version => 20100714134454) do
 
   create_table "favorites", :force => true do |t|
     t.string   "description"
