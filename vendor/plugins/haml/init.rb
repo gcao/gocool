18a2ba56ecb274a173be0ebd3660e2e44cc1cf4e
Guoliang Cao <gcao@vonage.com>
2010-07-15 11:03:34 -0400

    Util scripts

--- vendor/plugins/haml/init.rb
+++ vendor/plugins/haml/init.rb
@@ -1,17 +1,4 @@
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  begin
-    require 'haml' # From gem
-  rescue LoadError => e
-    # gems:install may be run to install Haml with the skeleton plugin
-    # but not the gem itself installed.
-    # Don't die if this is the case.
-    raise e unless defined?(Rake) &&
-      (Rake.application.top_level_tasks.include?('gems') ||
-        Rake.application.top_level_tasks.include?('gems:install'))
-  end
-end
+require 'haml'
 
 # Load Haml and Sass.
 # Haml may be undefined if we're running gems:install.
