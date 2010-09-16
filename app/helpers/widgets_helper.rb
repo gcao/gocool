a25faf4834a1c60aa6a3399786680067acd53f6b
Guoliang Cao <gcao99@gmail.com>
2010-09-16 11:14:13 -0400

    Fix syntax error

--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -8,10 +8,10 @@ module WidgetsHelper
   end
     
   def render_uploads uploads
-    render :partial => 'widgets/uploads', :locals => {:uploads => uploads.paginate page_params(:uploads_page)}
+    render :partial => 'widgets/uploads', :locals => {:uploads => uploads.paginate(page_params(:uploads_page))}
   end
   
   def render_opponents opponents
-    render :partial => 'widgets/opponents', :locals => {:opponents => opponents.paginate page_params(:opponents_page))
+    render :partial => 'widgets/opponents', :locals => {:opponents => opponents.paginate(page_params(:opponents_page))}
   end
 end
