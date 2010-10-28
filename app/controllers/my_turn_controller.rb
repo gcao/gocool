9a8c4045647fd2141b5a9ead7d66f2b4f457731c
Guoliang Cao <gcao99@gmail.com>
2010-10-28 09:51:42 -0400

    Change MyTurnController Add server upgrade planning notes

--- app/controllers/my_turn_controller.rb
+++ app/controllers/my_turn_controller.rb
@@ -1,13 +1,11 @@
 class MyTurnController < ActionController::Metal
-  include ActionController::Rendering
-
   def my_turn
     if Game.my_turn_by_name(request.params['player']).not_finished.count > 0
-      render :text => "my_turn"
+      self.response_body = "my_turn"
     elsif Game.by_name(request.params['player']).not_finished.count > 0
-      render :text => "not_my_turn"
+      self.response_body = "not_my_turn"
     else
-      render :text => "no_game"
+      self.response_body = "no_game"
     end
   end
 end
