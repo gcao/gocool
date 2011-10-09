ad1be7343b6765562403024fbd948d0b79861ecc
Guoliang Cao <gcao99@gmail.com>
2011-10-09 08:52:25 -0400

    Remove deprecated api call

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ Gocool::Application.configure do
   config.active_support.deprecation = :log
 
   # Show full error reports and disable caching
-  config.consider_all_requests_local       = true
   config.action_view.debug_rjs             = true
   config.action_controller.perform_caching = false
 
