0113121bb3bc4f528e3d2219dc8e9257e93c1d8e
Guoliang Cao <gcao99@gmail.com>
2011-11-26 11:47:39 -0500

    Deployment: get server name and username from environment variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,28 @@
+raise "Environment variable CHEF_SERVER is not set." unless ENV["CHEF_SERVER"]
+raise "Environment variable CHEF_USER is not set." unless ENV["CHEF_USER"]
+
 set :application, "gocool"
 set :deploy_to, "/data/apps/#{application}"
- 
+
 set :scm, :git
 set :repository, "git://github.com/gcao/gocool.git"
 set :branch, "rails3"
 set :git_enable_submodules, true
 set :git_submodules_recursive, false
 
-if ENV['DEPLOYMENT_TARGET'] == 'production'
-  set :user, "root"
-  set :use_sudo, false
-  
-  ami_host = `ami_host`.strip
-  # ami_host = `new_instance`.strip
-  
-  # AMI ami-0d729464: ubuntu 9.04 server base 
-  server ami_host, :app, :web, :db, :primary => true
-else
-  set :user, "vagrant"
-  set :use_sudo, true
-  set :runner, "root"
+set :user, ENV["CHEF_USER"]
+set :use_sudo, ENV["CHEF_USER"] != 'root'
+#set :runner, "root"  # Is this required when user is not root?
 
-  server 'vagrant', :app, :web, :db, :primary => true
-end
+server ENV["CHEF_SERVER"], :app, :web, :db, :primary => true
 
 namespace :deploy do
   task :start do
   end
-  
+
   task :stop do
   end
-  
+
   task :restart, :roles => :app, :except => { :no_release => true } do
     try_runner "touch #{current_path}/tmp/restart.txt"
   end
