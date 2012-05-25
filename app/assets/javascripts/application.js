b1164a04b34da7cdef57a4acb0ce5dd549b1e2cc
Guoliang Cao <gcao99@gmail.com>
2012-05-25 14:23:45 -0400

    Move assets to cool_games engine

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,47 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require jquery.autocomplete
-//= require jquery.date_input
-//= require jquery.form
-//= require validanguage_uncompressed
-//= require upload
-jQuery(document).ready(function(){
-    //arrowimages.down[1]="/assets/down.gif";
-    //arrowimages.right[1]="/assets/right.gif";
-    //jQuery('#myslidemenu .#' + controller_name).addClass('active');
+//= require cool_games
 
+jQuery(document).ready(function(){
     jQuery("#container input[name=reset]").click(function(){
         jQuery("#container form :input[type=text], #container form textarea").val("");
     });
 });
-
-function showError(fieldId, errorMsg) {
-    jQuery('#' + fieldId).addClass('failedField');
-    var errorMsgId = '#' + fieldId + "_errorMsg";
-    if (jQuery(errorMsgId).length > 0) {
-        jQuery(errorMsgId).text(errorMsg);
-    } else {
-        jQuery('#' + fieldId).after('<div class="vdError"><span id="' + fieldId + '_errorMsg">' + errorMsg + '</span></div>')
-    }
-}
-
-function showErrors(errors) {
-    jQuery.each(errors, function(i, value){
-        showError(value[0], value[1]);
-    });
-}
-
-function deleteGame(gameId) {
-    jQuery.post(gamesUrl + "/" + gameId, {
-        _method: 'delete'
-    });
-    jQuery('#game_row_' + gameId).remove();
-}
-
-function deletePlayer(playerId) {
-    jQuery.post(playersUrl + "/" + playerId, {
-        _method: 'delete'
-    });
-    jQuery('#player_row_' + playerId).remove();
-}
