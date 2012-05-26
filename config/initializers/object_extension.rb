8300571824b8968b07f37a94261045d7ab517459
Guoliang Cao <gcao99@gmail.com>
2012-05-26 10:10:47 -0400

    Remove object_extension, use cancan instead of nil_or

--- config/initializers/object_extension.rb
+++ config/initializers/object_extension.rb
@@ -1,23 +1,12 @@
-module ObjectExtension
-  class NilOr
-    def method_missing(sym, *args)
-
-    end
-  end
-
-  def not_nil?
-    !self.nil?
-  end
-  
-  def nil_or
-    return self unless self.nil?
-    NilOr.new
-  end
-
-  def not_blank?
-    !self.blank?
-  end
-
-end
-
-Object.send :include, ObjectExtension
+#module ObjectExtension
+#  def not_nil?
+#    !self.nil?
+#  end
+#
+#  def not_blank?
+#    !self.blank?
+#  end
+#
+#end
+#
+#Object.send :include, ObjectExtension
