e61cdb41e6c33637ee141ede6f54f0d4945adca3
Guoliang Cao <gcao99@gmail.com>
2012-05-26 10:34:37 -0400

    Undo last commit: cancan is the wrong gem to replace nil_or

--- config/initializers/object_extension.rb
+++ config/initializers/object_extension.rb
@@ -1,12 +1,23 @@
-#module ObjectExtension
-#  def not_nil?
-#    !self.nil?
-#  end
-#
-#  def not_blank?
-#    !self.blank?
-#  end
-#
-#end
-#
-#Object.send :include, ObjectExtension
+module ObjectExtension
+  class NilOr
+    def method_missing(sym, *args)
+
+    end
+  end
+
+  def not_nil?
+    !self.nil?
+  end
+
+  def nil_or
+    return self unless self.nil?
+    NilOr.new
+  end
+
+  def not_blank?
+    !self.blank?
+  end
+
+end
+
+Object.send :include, ObjectExtension
